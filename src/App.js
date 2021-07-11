eb9b31a2808897574386fe52e407e98a85c333b7
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-07-11 14:12:19 -0400

    Ready to add conversions

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
             <strong>This project coded by Jennifer Griffin and is</strong>
             <a href="https://github.com/coderGriff66/test-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
             <br />
-             <small>&emsp;Photo by www.paulvoie.com</small>
+            <small>&emsp;Photo by www.paulvoie.com</small>
           </footer>
         </div>
       </div>
-     </div>
+    </div>
   );
 }
 
